066a0ef7a601ed38f6e2fa6e8e812de242788250
mhussain6136 <muhammad.cs.204@gmail.com>
2023-10-07 05:09:19 +0500

    Add Xcode setup

---
+++ DesignStory/DesignStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignStory
+//
+//  Created by Muhammad Hussain on 07/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
